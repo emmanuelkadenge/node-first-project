1ba81f1d1c0dec4b4d729de0080fdd434de9308c
emmanuelkadenge <36509316+emmanuelkadenge@users.noreply.github.com>
2021-07-19 14:32:12 +0300

    undoing the app.js rename thing

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const http = require('http');
+const http = require('http')
+
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/') {
+    const url = req.url
+
+    if (url === '/'){
         res.write('<html>')
-        res.write('<head><title>first-node-project</title></head>')
-        res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></body>')
+        res.write('<head><title>first node server</title></head>')
+        res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Submit Data</button></form></body>')
         res.write('</html>')
         return res.end()
     } else {
-        res.setHeader('Content-Type','text/html')
+        res.setHeader('Content-Type', 'text/html')
         res.write('<html>')
-        res.write('<head><title>the fallback page</title></head>')
-        res.write('<body><h1>Welcome to the first Node Server!!!!</h1></body>')
+        res.write('<head><title>the alternative page</title></head>')
+        res.write('<body><h1>FIRST NODE SERVER !!!!!!!!!!</h1></body>')
         res.write('</html>')
         res.end()
     }

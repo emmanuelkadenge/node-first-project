c7ad97c8e1331096f7d01d0528309893d457ff98
emmanuelkadenge <36509316+emmanuelkadenge@users.noreply.github.com>
2021-07-30 08:23:13 +0300

    a little styling of the html pages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const  adminRouter = require('./router/admin');
@@ -6,13 +7,13 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.urlencoded({extended : false}));
 
 app.use('/admin',adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','error page.html'))
 })
 
 app.listen(3000)
